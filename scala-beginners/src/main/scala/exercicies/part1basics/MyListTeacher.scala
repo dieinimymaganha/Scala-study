a6e686a887f7a3b894c72453dddf68fd42eba32a
Dieinimy Maganha <dieinimy@gmail.com>
2023-03-18 15:23:35 -0300

    Exercicies Generics

--- scala-beginners/src/main/scala/exercicies/part1basics/MyListTeacher.scala
+++ scala-beginners/src/main/scala/exercicies/part1basics/MyListTeacher.scala
@@ -1,54 +1,67 @@
 package exercicies.part1basics
 
-abstract class MyListTeacher {
+import scala.NoSuchElementException
 
-  def head: Int
+abstract class MyListTeacher[+A] {
 
-  def tail: MyListTeacher
+  def head: A
+
+  def tail: MyListTeacher[A]
 
   def isEmpty: Boolean
 
-  def add(element: Int): MyListTeacher
+  def add[B >: A](element: B): MyListTeacher[B]
+
   def printElements: String
+
   override def toString: String = "[" + printElements + "]"
 }
 
 
-object Empty extends MyListTeacher {
-  def head: Int = throw new NoSuchElementException
+object Empty extends MyListTeacher[Nothing] {
 
-  def tail: MyListTeacher = throw new NoSuchElementException
+  def head: Nothing = throw new NoSuchElementException
+
+  def tail: MyListTeacher[Nothing] = throw new NoSuchElementException
 
   def isEmpty: Boolean = true
 
-  def add(element: Int): MyListTeacher = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyListTeacher[B] = new Cons(element, Empty)
+
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyListTeacher) extends MyListTeacher {
-  def head: Int = h
+class Cons[+A](h: A, t: MyListTeacher[A]) extends MyListTeacher[A] {
+  def head: A = h
 
-  def tail: MyListTeacher = t
+  def tail: MyListTeacher[A] = t
 
   def isEmpty: Boolean = false
 
-  def add(element: Int): MyListTeacher = {
-    new Cons(element, this)
-  }
+  def add[B >: A](element: B): MyListTeacher[B] = new Cons(element, this)
 
   def printElements: String =
     if (t.isEmpty) "" + h
-    else h + " " + t.printElements
-
+    else s"$h ${t.printElements}"
 
 }
+
+//object ListTest extends App {
+//  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+//  println(list.tail.head)
+//  println(list.add(4).head)
+//  println(list.add(4).head)
+//  println(list.toString)
+//
+//}
+//
+
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.add(4).head)
-  println(list.toString)
+  val listOfIntegers: MyListTeacher[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfString: MyListTeacher[String] = new Cons("Hello", new Cons("Scala", Empty))
+
+  println(listOfIntegers.toString)
+  println(listOfString.toString)
 
 }
-
