3b7fe321b68506f44f05d735e7c73013e589b499
Dieinimy Maganha <dieinimy@gmail.com>
2023-03-27 10:56:52 -0300

    Identify most Obscure Hero

---
+++ SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/MostObscureSuperheroDataset.scala
@@ -0,0 +1,64 @@
+package com.sundogsoftware.spark
+
+import org.apache.log4j._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+
+
+/** Find the superhero with the most co-appearances. */
+object MostObscureSuperheroDataset {
+
+  case class SuperHeroNames(id: Int, name: String)
+
+  case class SuperHero(value: String)
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession
+      .builder
+      .appName("MostObscureSuperHero")
+      .master("local[*]")
+      .getOrCreate()
+
+    // Create schema when reading Marvel-names.txt
+    val superHeroNamesSchema = new StructType()
+      .add("id", IntegerType, nullable = true)
+      .add("name", StringType, nullable = true)
+
+    // Build up a hero IID -> name Dataset
+    import spark.implicits._
+    val names = spark.read
+      .schema(superHeroNamesSchema)
+      .option("sep", " ")
+      .csv("data/Marvel-names.txt")
+      .as[SuperHeroNames]
+
+    val lines = spark.read
+      .text("data/Marvel-graph.txt")
+      .as[SuperHero]
+
+
+    val connections = lines
+      .withColumn("id", split(col("value"), " ")(0))
+      .withColumn("connections", size(split(col("value"), " ")) - 1)
+      .groupBy("id").agg(sum("connections").alias("connections"))
+
+
+    val minConnectionCount = connections.agg(min("connections")).first().getLong(0)
+
+    val minConnections = connections.filter($"connections" === minConnectionCount)
+
+    val minConnectionsWithNames = minConnections.join(names, usingColumn = "id")
+
+    println("The following characters have only " + minConnectionCount + " connections(s):")
+
+    minConnectionsWithNames.select("name").show()
+
+
+  }
+
+
+}
