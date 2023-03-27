6ab9e6998fae38c4df01d16d9903ddfeb07ba294
Dieinimy Maganha <dieinimy@gmail.com>
2023-03-27 09:40:35 -0300

    Created join with udf and broadcast in popular movies

--- SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/PopularMoviesDataset.scala
+++ SparkScalaCourse/src/main/scala/com/sundogsoftware/spark/PopularMoviesDataset.scala
@@ -2,14 +2,33 @@ package com.sundogsoftware.spark
 
 import org.apache.log4j._
 import org.apache.spark.sql._
-import org.apache.spark.sql.functions._
+import org.apache.spark.sql.functions.{col, udf}
 import org.apache.spark.sql.types.{IntegerType, LongType, StructType}
+
+import scala.io.{Codec, Source}
 
 /** Find the movies with the most ratings */
 object PopularMoviesDataset {
 
   // Case class so we can get a column name for our movie ID
-  final case class Movie(movieID: Int)
+  case class Movies(userID: Int, movieID: Int, rating: Int, timesTamp: Long)
+
+
+  def loadMovieNames(): Map[Int, String] = {
+    // Handle character enconding issues
+    implicit val codec: Codec = Codec("ISO-8859-1")
+
+    var movieNames: Map[Int, String] = Map()
+    val lines = Source.fromFile("data/ml-100k/u.item")
+    for (line <- lines.getLines()) {
+      val fields = line.split('|')
+      if (fields.length > 1) {
+        movieNames += (fields(0).toInt -> fields(1))
+      }
+    }
+    lines.close()
+    movieNames
+  }
 
   /** Our main function where the action happens */
   def main(args: Array[String]) {
@@ -31,6 +50,9 @@ object PopularMoviesDataset {
       .add("rating", IntegerType, nullable = true)
       .add("timestamp", LongType, nullable = true)
 
+
+    val nameDict = spark.sparkContext.broadcast(loadMovieNames())
+
     import spark.implicits._
 
     // Load up movie data as dataset
@@ -38,13 +60,33 @@ object PopularMoviesDataset {
       .option("sep", "\t")
       .schema(moviesSchema)
       .csv("data/ml-100k/u.data")
-      .as[Movie]
+      .as[Movies]
 
     // Some SQL-style magic to sort all movies by popularity in one line!
-    val topMoviesIDs = moviesDS.groupBy("movieID").count().orderBy(desc("count"))
+    val moviesCounts = moviesDS.groupBy("movieID").count()
 
-    // Grab the top 10
-    topMoviesIDs.show(10)
+    // Create a user-defined function to look up movie names from our
+    // shared Map variable
+
+    // We start by declaring an "anonymous function" in Scala
+    val lookupName: Int => String = (movieID: Int) => {
+      nameDict.value(movieID)
+    }
+
+    // Then wrap it with a udf
+    val lookupNameUDF = udf(lookupName)
+
+    // Add a movieTitle column using our new udf
+    val moviesWithNames = moviesCounts.withColumn("movieTitle", lookupNameUDF(col("movieId")))
+
+
+    // Sort the results
+
+    val sortedMoviesWithNames = moviesWithNames.sort("count")
+
+    // Show the results without truncating it
+    sortedMoviesWithNames.show(sortedMoviesWithNames.count.toInt, truncate = false)
+
     spark.stop()
 
 
